2fb35054ec08d32b655d5f0bd4206142b3e164fa
rafaeldevwp <fael.loliveira@outlook.com>
2023-02-12 15:57:53 -0300

    Inclusao Metodo Get

--- Controllers/FilesController.cs
+++ Controllers/FilesController.cs
@@ -11,13 +11,17 @@ public class FilesController : ControllerBase
     [HttpPost]
     public async Task<IActionResult> UploadFile(IFormFile file)
     {
-        if (file == null || file.Length == 0)
-        {
+        Guid Id = Guid.NewGuid();
+
+        if (file == null || file.Length == 0){
             return BadRequest();
-        }
+        }    
+        
+        //Atribuindo ID ao Arquivo Inserido
+        var ArquivoID = $"{Id}.{file.FileName}";
 
         //Determina onde o arquivo será salvo, apartir do diretorio corrente
-        var caminho = Path.Combine(Directory.GetCurrentDirectory(), "file_server", file.FileName);
+        var caminho = Path.Combine(Directory.GetCurrentDirectory(), "file_server", ArquivoID);
 
         //Atribui o caminho do diretorio ao Stream para criar o arquivo
         using (FileStream fs = new FileStream(caminho, FileMode.CreateNew))
@@ -31,7 +35,27 @@ public class FilesController : ControllerBase
 
         //Converte os Bytes em Base64
         var fileBase64 = Convert.ToBase64String(fileBytes);
+
         return Ok();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetFile([FromQuery] string Name)
+    {
+        if(String.IsNullOrEmpty(Name)){
+            return BadRequest();
+        }
+
+        var caminho = Path.Combine(Directory.GetCurrentDirectory(), "file_server");
+        var retorno =  Directory.GetFiles(caminho).Where(x => x.Contains(Name))?.FirstOrDefault();
+
+        Byte[] FileBytes;
+        var fileBase64 = "";
+
+            FileBytes = await System.IO.File.ReadAllBytesAsync(retorno);
+            fileBase64 = Convert.ToBase64String(FileBytes);
+
+        return Ok(fileBase64);
 
     }
 
