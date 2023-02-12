2af1c1bae00d8526d1e0b95cd20dc603ce3a0287
rafaeldevwp <fael.loliveira@outlook.com>
2023-02-12 08:28:53 -0300

    Criacao de Pasta e testes de upload

--- Controllers/FilesController.cs
+++ Controllers/FilesController.cs
@@ -5,13 +5,36 @@ namespace Estudos.Controllers;
 [ApiController]
 [Route("[controller]")]
 
-public class FilesController : ControllerBase {
+public class FilesController : ControllerBase
+{
 
-[HttpPost]
-public void PostFile(IFormFile file){
+    [HttpPost]
+    public async Task<IActionResult> UploadFile(IFormFile file)
+    {
+        if (file == null || file.Length == 0)
+        {
+            return BadRequest();
+        }
 
+        //Determina onde o arquivo será salvo, apartir do diretorio corrente
+        var caminho = Path.Combine(Directory.GetCurrentDirectory(), "file_server", file.FileName);
 
+        //Atribui o caminho do diretorio ao Stream para criar o arquivo
+        using (FileStream fs = new FileStream(caminho, FileMode.CreateNew))
+        {
+            //Copia o arquivo para o caminho preparado
+            await file.CopyToAsync(fs);
+        }
+
+        //Lendo os Bytes do arquivo
+        var fileBytes = System.IO.File.ReadAllBytes(caminho);
+
+        //Converte os Bytes em Base64
+        var fileBase64 = Convert.ToBase64String(fileBytes);
+        return Ok();
+
+    }
 
 }
 
-}
+
