3e74d2305221361151976c6c136474acd841110d
rafaeldevwp <fael.loliveira@outlook.com>
2023-02-20 21:12:22 -0300

    adicao de arquivos

--- Controllers/FilesController.cs
+++ Controllers/FilesController.cs
@@ -14,10 +14,11 @@ public class FilesController : ControllerBase
     {
         Guid Id = Guid.NewGuid();
 
-        if (file == null || file.Length == 0){
+        if (file == null || file.Length == 0)
+        {
             return BadRequest();
-        }    
-        
+        }
+
         //Atribuindo ID ao Arquivo Inserido
         var ArquivoID = $"{Id}.{file.FileName.ToLower()}";
 
@@ -40,38 +41,46 @@ public class FilesController : ControllerBase
         return Ok();
     }
 
-    [HttpGet (Name ="ObterArquivo")]
+    [HttpGet(Name = "ObterArquivo")]
     public async Task<IActionResult> GetFile([FromQuery] string Name)
     {
-        if(String.IsNullOrEmpty(Name)){
+        if (String.IsNullOrEmpty(Name))
+        {
             return BadRequest();
         }
-      
+
         var caminho = Path.Combine(Directory.GetCurrentDirectory(), "file_server");
-        var retorno =  Directory.GetFiles(caminho).Where(x => x.Contains(Name.ToLower()))?.FirstOrDefault();
+        var retorno = Directory.GetFiles(caminho).Where(x => x.Contains(Name.ToLower()))?.FirstOrDefault();
 
         Byte[] FileBytes;
         var fileBase64 = "";
 
-            FileBytes = await System.IO.File.ReadAllBytesAsync(retorno);
-            fileBase64 = Convert.ToBase64String(FileBytes);
+        FileBytes = await System.IO.File.ReadAllBytesAsync(retorno);
+        fileBase64 = Convert.ToBase64String(FileBytes);
 
         return Ok(fileBase64);
 
     }
 
     [HttpGet(Name = "ObterTodosArquivos")]
-    public IActionResult GetFiles(){
-        
-        var caminho = Path.Combine(Directory.GetCurrentDirectory(),"file_server");
-        var arquivos =  Directory.GetFiles(caminho).ToList();
-       
-        IList<string> ListaArquivos = new List<string>();
+    public IActionResult GetFiles()
+    {
 
-        arquivos.ForEach(a => {ListaArquivos.Add(a.Substring(16));});
+        var caminho = Path.Combine(Directory.GetCurrentDirectory(), "file_server");
+        var arquivos = Directory.GetFiles(caminho).ToList();
+
+        IList<string> ListaArquivos = new List<string>();
+        var guidLength = 0;
+       
+
+        arquivos.ForEach(a =>
+        {
+            guidLength = 54;
+            ListaArquivos.Add(a.Substring(guidLength));
+        });
 
         var json = JsonConvert.SerializeObject(ListaArquivos);
-        
+
         return Ok(json);
     }
 
---
+++ Controllers/ParalellController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Estudos.Controllers;
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+
+public class ParalellController : ControllerBase
+{
+    
+
+}
